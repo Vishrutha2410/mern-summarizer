6b2c13f1791bad7d456cdc6c79327f65a11ecef0
Vishrutha N K B <vishruthabalu06@gmail.com>
2026-03-04 03:55:05 +0530

    Updated code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const token = localStorage.getItem("token");
+  
   return (
     <BrowserRouter>
       <Routes>
